fa06c9330d89966ee864048c67e4b85c6971f6b3
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-12-22 16:29:59 +0000

    stricter bash options, better killing of ssh connections

--- collect.sh
+++ collect.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+set -o errexit; set -o pipefail; set -o nounset
+trap "kill 0" EXIT
+
 if [ "$#" -lt 1 ]; then
     echo "please pass some hosts to connect to"
     exit 1
 fi
 
 for SERVER in $@; do
-    ssh $SERVER "ss -H -n -tipa state connected exclude time-wait exclude fin-wait-2" |
+    ssh -tt $SERVER "ss -H -n -tipa state connected exclude time-wait exclude fin-wait-2" |
     sed "s/127.0.0.1/$SERVER/" > ss-$SERVER.txt &
     #replace local IP address with the external IP address we use to SSH in
     #this might work if you're SSHing in on the same interface as the connections you want to monitor
