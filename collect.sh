2a589660147935fc83b9ea660b52c07e1c2bfc6c
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-12-22 16:27:41 +0000

    replace parallel with for loop so we can output to different files, merge them, and replace localhost with SSH host

--- collect.sh
+++ collect.sh
@@ -5,4 +5,13 @@ if [ "$#" -lt 1 ]; then
     exit 1
 fi
 
-parallel ssh {} "ss -H -n -tipa state connected exclude time-wait exclude fin-wait-2" ::: $@ > ss.txt
+for SERVER in $@; do
+    ssh $SERVER "ss -H -n -tipa state connected exclude time-wait exclude fin-wait-2" |
+    sed "s/127.0.0.1/$SERVER/" > ss-$SERVER.txt &
+    #replace local IP address with the external IP address we use to SSH in
+    #this might work if you're SSHing in on the same interface as the connections you want to monitor
+done
+
+wait
+
+cat ss-*.txt > ss.txt
