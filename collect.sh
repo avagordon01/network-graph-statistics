d364f1ded589d22b42a2928b45cae01396451d76
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-12-22 22:49:20 +0000

    add global flag to collect host replace regex

--- collect.sh
+++ collect.sh
@@ -10,7 +10,7 @@ fi
 
 for SERVER in $@; do
     ssh -tt $SERVER "ss -H -n -tipa state connected exclude time-wait exclude fin-wait-2" |
-    sed "s/127.0.0.1/$SERVER/" > ss-$SERVER.txt &
+    sed "s/127.0.0.1/$SERVER/g" > ss-$SERVER.txt &
     #replace local IP address with the external IP address we use to SSH in
     #this might work if you're SSHing in on the same interface as the connections you want to monitor
 done
