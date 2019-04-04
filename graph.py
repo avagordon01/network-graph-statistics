9387279728b7578824aa94e0a80c42e9a7c72afe
Patrick Gordon <pgpatrickgordon@gmail.com>
2019-04-04 10:34:28 +0100

    remove graphing from python, just output json for input to d3

--- graph.py
+++ graph.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 
-import networkx as nx
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 import pickle
+import json
 
 def main():
     connections = pickle.load(open('connections.pickle', 'rb'))
@@ -15,39 +12,27 @@ def main():
         if connection['peer_addr'] and connection['peer_pid']:
             address_pid_map[connection['peer_addr']] = connection['peer_pid']
 
+    print(address_pid_map)
+    nodes = []
     edges = []
     for connection in connections:
         #smaller weight = further apart
         connection['weight'] = 1 / connection['rtt_avg']
-        interested = False
-        for user in connection['users']:
-            interested = user['name'] in ['1', '2', '3', '4', '5', '6']
-            if interested:
-                break
-        interested = interested and connection['users']
-        if not interested:
+        if not connection['users']:
             continue
         local_pid = address_pid_map[connection['local_addr']]
         if connection['peer_addr'] in address_pid_map:
             peer_pid = address_pid_map[connection['peer_addr']]
-            edge = (local_pid, peer_pid, connection)
+            edges.append({'source': str(local_pid), 'target': str(peer_pid), **connection})
+            nodes.append({'id': str(peer_pid)})
         else:
-            edge = (local_pid, connection['peer_addr'], connection)
-        edges.append(edge)
+            edges.append({'source': str(local_pid), 'target': connection['peer_addr'], **connection})
+            nodes.append({'id': str(connection['peer_addr'])})
+        nodes.append({'id': str(local_pid), **connection['users'][0]})
 
-    G = nx.MultiGraph()
-    G.add_edges_from(edges)
-
-    pos = nx.layout.spring_layout(G)
-
-    nx.draw_networkx_nodes(G, pos, node_size=1)
-    nx.draw_networkx_edges(G, pos, width=1)
-    #nx.draw_networkx_labels(G, pos, font_size=1, font_family='sans-serif')
-
-    plt.axis('off')
-    plt.gcf().set_size_inches(32, 24)
-    plt.savefig('graph.png', dpi=96)
-    plt.show()
+    json_graph = {'nodes': nodes, 'links': edges}
+    with open('data.json', 'w') as outfile:
+        json.dump(json_graph, outfile)
 
 if __name__ == '__main__':
     main()
